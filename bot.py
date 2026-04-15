3b452aa56cff06561d852b70ea3533e248aac8d8
aitoolsdx-glitch <aitoolsdx@gmail.com>
2026-04-15 22:36:04 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -44,11 +44,8 @@ async def attack_logic(url, threads, message):
 
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
-    await message.answer("🤖 CHIIP на связи! Используй /dos <url> <потоки> для теста на уязвимости. 
-Чтобы тест запустился, нужно писать команду строго в таком виде:
-/dos [URL] [КОЛИЧЕСТВО_ПОТОКОВ]
-Например:
-/dos <ссылка> 50")
+    await message.answer("🤖 CHIIP на связи! Используй /dos -url- -потоки- для теста на уязвимости. Чтобы тест запустился, нужно писать команду строго в таком виде: /dos [URL] [КОЛИЧЕСТВО_ПОТОКОВ]
+Например: /dos -ссылка- 50")
 
 @dp.message(Command("dos"))
 async def cmd_dos(message: types.Message):
